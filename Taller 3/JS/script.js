8b5f21c2a34478f320d4e9d7ce96bcf1df93eb60
SALA DE ACCESO LIBRE <SALAAL@EAN2651.universidad.ean>
2019-02-19 17:58:30 -0500

    Cambios en Taller 3

--- Taller 3/JS/script.js
+++ Taller 3/JS/script.js
@@ -1,6 +1,29 @@
-function calcularIva(){
-    var valorB = document.getElementById("valorB").value.parseInt;
-    var valorI = document.getElementById("valorI").value.parseInt;
-    var resultado = document.getElementById("valorT")
-    resultado.innerHTML = ((parseInt(valorB)*parseInt(valorI)/100)+parseInt(valorB));
-}
+var boton1 = document.getElementById("calcularIva")
+boton1.onclick = function calcularIva(){
+    var valorB = document.getElementById("valorB").value;
+    var valorI = document.getElementById("valorI").value;
+    var resultado = document.getElementById("valorT");
+    resultado.innerHTML = (((parseInt(valorB)*parseInt(valorI))/100)+parseInt(valorB));
+};
+
+var boton2 = document.getElementById("leerCadena")
+
+boton2.onclick = function leerCadena(){
+    var cad = document.getElementById("cad1");
+    var numChars = document.getElementById("numChars");
+    var invCad = document.getElementById("invCad");
+    var invCadMayus = document.getElementById("invCadMayus");
+
+    function test(){
+        var x = cad.value.length;
+        var cadInver ="";
+        while (x>=0){
+            cadInver = cadInver+cad.value.charAt(x);
+            xs--;
+        }
+        return cadInver;
+    }
+
+    numChars.innerHTML =  cad.value.length;
+    invCad.innerHTML = test();
+};
